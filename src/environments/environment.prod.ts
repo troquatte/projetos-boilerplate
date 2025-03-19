db4a455c08d43ee844c91e8e1868e9c6f9e4a4e9
Dener Troquatte <d.troquatte20@yahoo.com.br>
2025-03-19 18:14:55 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     projectId: String(process.env['PROJECT_ID']),
     storageBucket: String(process.env['STORAGE_BUCKET']),
     messagingSenderId: String(process.env['MESSAGING_SENDER_ID']),
-    appId: String(process.env['API_ID']),
+    appId: String(process.env['APP_ID']),
     measurementId: String(process.env['MEASUREMENT_ID']),
   },
   cert: {
